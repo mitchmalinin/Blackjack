390169039588cdc2568153ba5da98e0b8939d3ce
Mitch Malinin <mitchmalinin@gmail.com>
2019-06-23 14:09:40 -0400

    fixed buttons again

--- JavaScript/card.js
+++ JavaScript/card.js
@@ -1,5 +1,4 @@
-class Deck {
-  constructor(card) {
-    this.card = card;
-  }
-}
+let cards = [
+  { suit: "s1", name: "c1", value: "6", image: "" },
+  { suit: "s1", name: "c2", value: "7", image: "" }
+];
